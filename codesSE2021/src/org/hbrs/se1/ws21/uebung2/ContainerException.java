0b98b0d7b9c09218612d17883176d8708ba6c0e3
YareYareO <92210481+YareYareO@users.noreply.github.com>
2021-10-19 19:28:29 +0200

    Delete codesSE2021/src/org/hbrs/se1/ws21/uebung2 directory

--- codesSE2021/src/org/hbrs/se1/ws21/uebung2/ContainerException.java
+++
@@ -1,7 +0,0 @@
-package org.hbrs.se1.ws21.uebung2;
-
-public class ContainerException extends Exception{
-    public ContainerException(){
-        super("Das Member-Objekt mit der ID ist bereits vorhanden!");
-    }
-}
