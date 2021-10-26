c74a1a78af31589e7de29395380a2d84ede27603
YareYareO <92210481+YareYareO@users.noreply.github.com>
2021-10-26 21:31:28 +0200

    Delete codesSE2021/src/org/hbrs/se1/ws21/uebung3/persistence directory

--- codesSE2021/src/org/hbrs/se1/ws21/uebung3/persistence/ContainerException.java
+++
@@ -1,7 +0,0 @@
-package org.hbrs.se1.ws21.uebung3.persistence;
-
-public class ContainerException extends Exception{
-    public ContainerException(Integer id){
-        super("Das Member-Objekt mit der ID: " + id + "ist bereits vorhanden!");
-    }
-}
--- codesSE2021/src/org/hbrs/se1/ws21/uebung3/persistence/Main.java
+++
@@ -1,9 +0,0 @@
-package org.hbrs.se1.ws21.uebung3.persistence;
-
-public class Main {
-    public static void setAContainersStrategy(Container c, PersistenceStrategy<Member> p){
-        c.setPS(p);
-    }
-
-
-}
