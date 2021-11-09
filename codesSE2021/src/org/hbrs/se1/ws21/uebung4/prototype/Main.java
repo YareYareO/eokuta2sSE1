a2c1f55b627b4b339b229ec54506f88faf31ed72
YareYareO <92210481+YareYareO@users.noreply.github.com>
2021-11-09 23:09:00 +0100

    Add files via upload

---
+++ codesSE2021/src/org/hbrs/se1/ws21/uebung4/prototype/Main.java
@@ -0,0 +1,15 @@
+package org.hbrs.se1.ws21.uebung4.prototype;
+
+public class Main {
+
+    /** Keine Testmethode aber machen sie mit der main methode was sie wollen!!! Diese startet dei eingabe und alle funktion sollten drinne sein mit Exceptions etc*/
+    public static void main (String[] args) throws Exception {
+        PersistenceStrategyStream<Employee> pss = new PersistenceStrategyStream<>();
+        Container con = Container.getInstance();
+        Utility.setAContainersStrategy(con, pss);
+        Eingabe eingabe = new Eingabe(con);
+
+        eingabe.startEingabe();
+
+    }
+}
